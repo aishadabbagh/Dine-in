63a90584f6a0c48f1ebe88dac9317affdda53abd
Lamees AlFallaj <lafallaj2@gmail.com>
2019-11-24 13:46:08 +0300

    fix the model name

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
          has_many :reservations, dependent: :destroy
-         belongs_to :users
+         belongs_to :user
          has_many :foods, dependent: :destroy
          has_many :tables, dependent: :destroy
          has_many :chefs, dependent: :destroy
