5a30890571fefb38d75f5984567a127468118899
Lamees AlFallaj <lafallaj2@gmail.com>
2019-11-24 11:34:33 +0300

    updated database

--- db/migrate/20191123085443_create_orders.rb
+++ db/migrate/20191123085443_create_orders.rb
@@ -4,6 +4,9 @@ class CreateOrders < ActiveRecord::Migration[5.2]
       t.string :status
       t.references :reservation, foreign_key: true
       t.references :chef, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :restaurant, foreign_key: true
+
 
       t.timestamps
     end
