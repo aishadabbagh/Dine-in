fac8df3806c28fa094f9d5c17b4ea6a013d54357
Lamees AlFallaj <lafallaj2@gmail.com>
2019-11-25 23:03:40 +0300

    change the root to restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
-  root to: "home#index"
+  root "restaurants#index"
   resources :reservations, :restaurants, :orders, :foods
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
