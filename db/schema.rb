4adbcfb67a465a3248f3d6f433b7b5d39301d520
Lamees AlFallaj <lafallaj2@gmail.com>
2019-11-25 20:59:37 +0300

    add column to restaurant table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_113115) do
+ActiveRecord::Schema.define(version: 2019_11_25_165349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_113115) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
