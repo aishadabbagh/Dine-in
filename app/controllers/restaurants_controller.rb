1bf79561a006d0e5e09bc896ff870987eabe69ae
Lamees AlFallaj <lafallaj2@gmail.com>
2019-11-25 22:04:35 +0300

    add the new function

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,16 @@
 class RestaurantsController < ApplicationController
+
     def index 
     @restaurants = Restaurant.all    
     end
 
     def show 
     @restaurant = Restaurant.find(params[:id])
-
     end
+
+    def new
+        @restaurant = Restaurant.new
+        # @restaurant_id = params
+    end
+
 end
