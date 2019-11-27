b4142c8d4d17ca4ff44f5ccbdb621eb5747dd1a6
aishadabbagh <aisha.dabbagh@gmail.com>
2019-11-27 09:11:06 +0300

    Added comments on controllers and seed File

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
-
+# creating devise
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+
+# Creating roles for users ???????  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:Name, :Phone_Number, :Role, :Wallet])
